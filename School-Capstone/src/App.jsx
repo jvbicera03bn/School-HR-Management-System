97ae893b343f0d387e5a1f8d8c441d5528e8827c
jvbicera03bn <jvbicera03.bn@gmail.com>
2023-03-02 10:42:53 +0800

    asd

--- School-Capstone/src/App.jsx
+++ School-Capstone/src/App.jsx
@@ -1,12 +1,27 @@
 import './App.css'
 
-function App() {
 
-  return (
-    <div className="App">
+const App = () => {
+    return (
+        <div className="App">
+            <div className='side_bar'>
+                <div className='profile_info'>
+                    <img className='profile_picture' src="./img/profile-placeholder.jpeg" alt="profile" />
+                    <h3>Juan Dela Cruz</h3>
+                    <p>Human Resource</p>
+                </div>
+                <div className='school_name'>
+                    <h2>ASIATECH</h2>
+                    <p>Human Resource Management System</p>
+                </div>
+            </div>
+            <div className='content_window'>
+                <div className='main_content'>
 
-    </div>
-  )
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default App
